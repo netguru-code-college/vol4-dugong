1419859e321e9fea45540b828bfe97cf9e2f6a86
Michal Drawc <pistolisloaded@gmail.com>
2019-02-17 00:10:24 +0100

    Add footer, add links, add story model, add stories controller, add routes, modify application controller for login purposes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home' 
+  root 'static_pages#home'
   devise_for :users
+
+  resources :users do
+    resources :stories
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
-
