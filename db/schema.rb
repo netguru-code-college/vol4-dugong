e04b5c57b73095a968bea55dd8ce84c75d0f40b7
jjkazm <jjkazm@gmail.com>
2019-02-18 12:55:06 +0100

    Install friendly_id gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_101515) do
+ActiveRecord::Schema.define(version: 2019_02_18_113841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_02_18_101515) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
