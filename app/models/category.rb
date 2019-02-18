8e4c88e0b31ced202fcd8ea8bb87e64d3fd45ebd
jjkazm <jjkazm@gmail.com>
2019-02-18 14:02:17 +0100

    Use friendly_id to display category routes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   ##### Uncomment  after story model is added
   # has_many :stories
   validates :name, inclusion: { in: %w(bad good) }
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 end
