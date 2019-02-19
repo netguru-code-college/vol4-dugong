0d92bfbff3256c7fbd65eadbcfb273e050eeae5c
Michal Drawc <pistolisloaded@gmail.com>
2019-02-19 18:05:13 +0100

    Add random pictures to stories in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,26 +36,37 @@ bad_titles = [
   "Just my luck", "Sutpid Boss", "Dead Cat"
 ]
 
+ar_gdpath = []
+ar_bdpath = []
+path = 'app/assets/images/'
+num_of_pictures = 8
+num_of_pictures.times do |i|
+  ar_gdpath[i] = [path + 'good/' + (i+1).to_s + '.jpg', (i+1).to_s + '.jpg']
+  ar_bdpath[i] = [path + 'bad/' + (i+1).to_s + '.jpg', (i+1).to_s + '.jpg']
+end
+
 #BADSTORIES:
 User.all.each do |user|
     rand(2..10).times do
-    story = Story.new
-    story.title = bad_titles.sample
-    story.text = Faker::Quotes::Shakespeare.hamlet_quote
-    story.picture = nil
-    story.user_id = user.id
-    story.category_id = 1
-    story.save!
+      rork = rand(0...num_of_pictures)
+      story = Story.new
+      story.title = bad_titles.sample
+      story.text = Faker::Quotes::Shakespeare.hamlet_quote
+      story.picture.attach(io: File.open(ar_bdpath[rork].first), filename: ar_bdpath[rork].second, content_type: 'image/jpg')
+      story.user_id = user.id
+      story.category_id = 1
+      story.save!
   end
 end
 
 #GOODSTORIES:
 User.all.each do |user|
     rand(1..5).times do
+    rork = rand(0...num_of_pictures)
     story = Story.new
     story.title = good_titles.sample
     story.text = Faker::Movies::Lebowski.quote
-    story.picture = nil
+    story.picture.attach(io: File.open(ar_gdpath[rork].first), filename: ar_gdpath[rork].second, content_type: 'image/jpg')
     story.user_id = user.id
     story.category_id = 2
     story.save!
@@ -65,6 +76,6 @@ end
 #Comments
 Story.all.each do |story|
   rand(10).times do
-    story.comments.create(body: Faker::TvShows::SouthPark.quote, user_id: rand(20))
+    story.comments.create(body: Faker::TvShows::SouthPark.quote, user_id: rand(1..User.count))
   end
 end
