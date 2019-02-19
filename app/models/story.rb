a2dea2d2f7b59004825fb5d646d99728cbc698ae
jjkazm <jjkazm@gmail.com>
2019-02-19 10:50:56 +0100

    Add comment model and associations with story and user

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :stories
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :followers, as: :followable
 end
