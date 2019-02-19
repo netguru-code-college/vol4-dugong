ce22e65c84610fc2a4b43e7b2c220eaf20600ff2
jjkazm <jjkazm@gmail.com>
2019-02-19 15:32:00 +0100

    Creating comment working state

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
 
   resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :categories, only: :show, path: "" do
     resources :stories do
       resources :comments, only: :create
     end
   end
+
+
 
 end
