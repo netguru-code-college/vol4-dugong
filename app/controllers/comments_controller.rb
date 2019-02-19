607923b0551ed9b851c417afcbda38f7506e1cb4
jjkazm <jjkazm@gmail.com>
2019-02-19 12:15:04 +0100

    Config comments routes and fix comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,17 @@ class CommentsController < ApplicationController
     @story = Story.find(params[:id])
     @comment = @story.comments.new(comment_params)
     @comment.user = current_user
+    if @comment.save
+      flash[:success] = "Comment added succesfully"
+    else
+      flash[:danger] = "#{@comment.errors.full_messages.first}"
+    end
+    redirect_to story_path(@story)
   end
 
   private
-  def comment_params
-    params.require(:comment).permit(:body, :user_id, :story_id)
-  end
+    def comment_params
+      params.require(:comment).permit(:body, :user_id, :story_id)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :categories, only: :show, path: "" do
-    resources :stories
+    resources :stories do
+      resources :comments, only: :create
+    end
   end
 
 end
