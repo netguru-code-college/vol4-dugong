df131e1233dc049afa4558a41e33437d734b99c1
jjkazm <jjkazm@gmail.com>
2019-02-20 12:04:39 +0100

    Test vote model associations

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Story, type: :model do
     it { should have_many(:comments) }
+    it { should have_many(:votes) }
     it { should belong_to(:user) }
     it { should belong_to(:location) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should have_many(:followers) }
-  it { should have_many(:comments) }
+  it { should have_many(:votes) }
 end
