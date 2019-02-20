fa217b4aa90c0d9f94907a87ad23ed9287aea743
jjkazm <jjkazm@gmail.com>
2019-02-20 11:56:05 +0100

    Add association between location and story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :location
   has_one_attached :picture
   has_many :comments
   has_many :votes, dependent: :destroy
