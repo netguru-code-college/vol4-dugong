9fb84b82c402e8b950f301a6c7575b57c307f8af
jjkazm <jjkazm@gmail.com>
2019-02-20 13:07:43 +0100

    Create uniq location - constant params

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,16 +3,25 @@ class StoriesController < ApplicationController
 
 def new
   @story = Story.new
+  @location = Location.new
 end
 
 def show
+
   @story = Story.find(params[:id])
   @comments = @story.comments
 end
 
 def create
+  @lati = 53.1235
+  @longi = 18.0084
+  @location = Location
+                .where(latitude: @lati)
+                .where(longitude: @longi)
+                .first || Location.create(latitude: @lati, longitude: @longi)
   @story = current_user.stories.build(story_params)
   @story.category = @category
+  @story.location = @location
 
   if @story.save
     flash[:success] = "Story was successfully added. Look for it below. :) "
@@ -25,7 +34,7 @@ end
 
 private
   def story_params
-    params.require(:story).permit(:title, :text, :category, :picture, :user_id)
+    params.require(:story).permit(:title, :text, :category, :picture, :user_id, :location_id)
   end
 
   def set_category
