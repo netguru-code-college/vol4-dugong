96323f742f23722d4839089e12ce04c3f4a61a84
Michal Drawc <pistolisloaded@gmail.com>
2019-02-20 12:24:05 +0100

    Add votes logic, update view, no vote/comment option for not logged users

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,16 +2,35 @@ class VotesController < ApplicationController
   before_action :find_story
 
   def create_up
-    @story.votes.create(user_id: current_user.id, updown: true)
-    redirect_to root_path
+    if already_voted?
+      flash[:notice] = "You can't vote more than once"
+    else
+      flash[:success] = "Thanks for voting"
+      @story.votes.create(user_id: current_user.id, updown: true)
+    end
+
+    redirect_to category_story_path(Category.find(params[:category_id]), @story.id)
+
   end
 
   def create_down
-    @story.votes.create(user_id: current_user.id, updown: false)
-    redirect_to root_path
+    if already_voted?
+      flash[:notice] = "You can't vote more than once"
+    else
+      flash[:success] = "Thanks for voting"
+      @story.votes.create(user_id: current_user.id, updown: false)
+    end
+
+    redirect_to category_story_path(Category.find(params[:category_id]), @story.id)
+
   end
 
   private
+
+  def already_voted?
+    Vote.where(user_id: current_user.id, story_id:
+    params[:story_id]).exists?
+  end
 
   def find_story
     @story = Story.find(params[:story_id])
